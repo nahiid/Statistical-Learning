2df4df8a64e2d06f42a60287c64cbf458d0e2a9a
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-01-31 17:02:37 -0500

    Find corr between all numerical columns

--- multiple_linear_regression_auto_dataset/A1_B2.r
+++ multiple_linear_regression_auto_dataset/A1_B2.r
@@ -1,9 +1,34 @@
-library(ISLR)
+library(ISLR2)
 library(ggplot2)
 library(GGally)
+
+#_________________PART A________________________
+#_______________________________________________
+# Goal: Find Scatterplot of all variables
 
 # Method 1: Use Base R
 plot(Auto, pch=20, cex=1.5, col='steelblue')
 
 # Method 2: Use ggplot2 and GGally packages
 ggpairs(Auto, cardinality_threshold = 304)
+
+# Method 3: Base R
+pairs(Auto);
+
+#_________________PART B_______________________
+#______________________________________________
+# Goal: Find Correlation between numerical columns
+
+# Method 1: Find cor() for numeric variables
+cor(Auto[sapply(Auto, is.numeric)])
+
+# Method 2 : Find and exclude the qualitative column and then use cor()
+index <- which(colnames(Auto) == 'name')
+modified_auto <- Auto[ , -index ]
+cor(modified_auto)
+
+#_________________PART C_______________________
+#______________________________________________
+# Goal: With lm() perform a multiple linear regression 
+# with mpg as the response and all other variables except name as
+# the predictors.
