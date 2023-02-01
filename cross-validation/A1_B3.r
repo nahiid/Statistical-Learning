10cce2f978fc6275abedc24ced36e9fd5767759a
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-02-01 17:47:12 -0500

    Generate sthe data

---
+++ cross-validation/A1_B3.r
@@ -0,0 +1,26 @@
+
+
+
+#_________________PART A________________________
+#_______________________________________________
+# Goal: Generate a simulated data set
+# i. In this data set, what is n and what is p? 
+# ii. Write out the model used to generate the data in equation form.
+
+set.seed (1)
+# rnorm(n, mean = 0, sd = 1)
+x <- rnorm (100)
+y <- x - 2 * x^2 + rnorm (100)
+
+#--------------[i]----------------
+# n is the number of the observation and here we created a dataset with 100 observations.
+# So n is equal to 100
+# p is the number of predictors. here predictors are x and x^2
+# So p is equal to 2
+
+#--------------[ii]----------------
+# The model = x - 2 + 2*x^2 + error here rnorm(100) play the role of error that is a small number.
+# We can write it in this way: f(x) = x - 2 + 2*x^2 
+# So the model= Y = f(x) + error
+# We know this error is irreducible which has the mean zero and sd=1 and they have a Normal dist.
+# becuse we made it that way with rnorm function :)
