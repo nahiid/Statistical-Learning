4699cb5ffc96ce2a6f276e2e256121ec5e3c7024
Nahid Aghababaeyan <nahid.aut@gmail.com>
2023-02-15 19:14:13 -0500

    Generate data

--- MSELoocv/A2_B1.r
+++ MSELoocv/A2_B1.r
@@ -4,3 +4,14 @@
 #mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
 
 # Goal: Validate MSELoocv closed formula
+
+set.seed(1)
+
+#Simulate n = 100 observations from the model Y = 2 + 3X + e where X, e are both i.i.d. N(0, 1)
+n = 100
+x = rnorm(n)
+e = rnorm(n)
+y = 2 + 3*x + e
+
+# Creating data frame from the generated data
+df = data.frame(cbind( y,x))
